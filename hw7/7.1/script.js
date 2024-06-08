1b67e3ad31fe6c001ce66cccf31f3d7a87a6a86d
AlexHolavitsky <alex65870@gmail.com>
2024-06-08 13:00:48 +0300

    08june_commit

---
+++ hw7/7.1/script.js
@@ -0,0 +1,22 @@
+
+// ДЗ 7.1. Замикання
+// Написати функцію, яка приймає 1 параметр.
+//  Та скадае значення з тим, що передали перший раз і т. д. Все це із замиканнями, наприклад:
+// console.log(sum(4)); // 4
+// console.log(sum(6)); // 10
+// console.log(sum(10)); // 20
+// console.log(sum(7)); // 27
+function createSumFunction() {
+    let sum = 0;
+    return function(number) {
+      sum += number;
+      return sum;
+    };
+  }
+  
+  const sum = createSumFunction();
+  
+  console.log(sum(4)); // 4
+  console.log(sum(6)); // 10
+  console.log(sum(10)); // 20
+  console.log(sum(7)); // 27
