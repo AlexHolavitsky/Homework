68dd89603f288b888bba74e8e32f16cf84b3f087
AlexHolavitsky <alex65870@gmail.com>
2024-06-08 13:56:57 +0300

    08june2_commit

--- hw7/7.2/script.js
+++ hw7/7.2/script.js
@@ -2,11 +2,21 @@
 // ДЗ 7.2. Добуток через карування
 // Створити функцію для розрахунку добутку двох чисел, що викликається так: name(5)(2). 
 // Функція повинна повертати результат (у середині функції не має бути консоль лога!)
-function NAME(x) {
-    return function(y) {
-        return x * y;
-    };
-}
 
-const result = NAME(5)(2);
-console.log(result); // 10
+// 1й вариант
+// function NAME(x) {
+//     return function(y) {
+//         return x * y;
+//     };
+// }
+// const result = NAME(5)(2);
+// console.log(result); // 10
+
+
+// 2й вариант
+function name(a) {
+    return(b) =>{
+        return a * b
+    }
+}
+console.log(name(5)(2))
