3471335a692ad2a212b5932985a9d3877920a56d
AlexHolavitsky <alex65870@gmail.com>
2024-06-08 14:15:29 +0300

    8june3_commit

--- hw7/7.1/script.js
+++ hw7/7.1/script.js
@@ -12,10 +12,10 @@ function createSumFunction() {
       sum += number;
       return sum;
     };
-  }
+}
   const sum = createSumFunction();
 
-  console.log(sum(4)); // 4
-  console.log(sum(6)); // 10
-  console.log(sum(10)); // 20
-  console.log(sum(7)); // 27
+console.log(sum(4)); // 4
+console.log(sum(6)); // 10
+console.log(sum(10)); // 20
+console.log(sum(7)); // 27 
